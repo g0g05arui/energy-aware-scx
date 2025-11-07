9b486f6840901f7d0aaad32f63b3995cb1f372ac
Mihai <mihai.indreias@epfl.ch>
2025-11-07 15:41:14 +0000

    first commit

---
+++ src/include/rapl_stats.h
@@ -0,0 +1,27 @@
+#ifndef RAPL_STATS_H
+#define RAPL_STATS_H
+
+#define energy_t unsigned long long
+#define power_t unsigned long long
+#define time_t_ns unsigned long long
+
+
+struct rapl_stats{
+    // Power consumption (in watts)
+    power_t package_power;       // Total package power
+    power_t core_power;          // CPU cores power
+    
+    energy_t package_energy;     // Total package energy consumed
+    energy_t core_energy;        // CPU cores energy consumed
+    
+    // Temperature (in degrees Celsius)
+    unsigned int package_temp;   // Package temperature
+    unsigned int core_temp;      // Average core temperature
+    
+    time_t_ns timestamp;         // Time of measurement
+    time_t_ns delta_time;        // Time elapsed since last measurement
+    
+    power_t tdp;                 
+};
+
+#endif
