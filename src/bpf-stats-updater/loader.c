007021b78708da54f1f7e50f58b65a802db64206
Mihai Indreias <mihai.indreias@epfl.ch>
2025-11-24 13:36:24 +0000

    test rapl

--- src/bpf-stats-updater/loader.c
+++ src/bpf-stats-updater/loader.c
@@ -4,63 +4,171 @@
 #include <unistd.h>
 #include <signal.h>
 #include <errno.h>
+#include <string.h>
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include <sys/resource.h>
-#include <time.h>
+#include <linux/perf_event.h>
+#include <sys/syscall.h>
+#include <asm/unistd.h>
+#include <limits.h>
 #include "../include/rapl_stats.h"
+
+#define PERF_IDX_PACKAGE 0
+#define PERF_IDX_CORE    1
+#define PERF_EVENT_COUNT 2
 
 static volatile int keep_running = 1;
 
-void sig_handler(int signo) {
+static void sig_handler(int signo)
+{
     keep_running = 0;
 }
 
-int main(int argc, char **argv) {
+static int perf_event_open(struct perf_event_attr *attr, pid_t pid, int cpu,
+                           int group_fd, unsigned long flags)
+{
+    return syscall(__NR_perf_event_open, attr, pid, cpu, group_fd, flags);
+}
+
+static int read_file_u64(const char *path, unsigned long long *value)
+{
+    FILE *f = fopen(path, "r");
+    if (!f)
+        return -errno;
+
+    if (fscanf(f, "%llu", value) != 1) {
+        fclose(f);
+        return -EIO;
+    }
+
+    fclose(f);
+    return 0;
+}
+
+static int read_event_config(const char *path, unsigned long long *config)
+{
+    FILE *f = fopen(path, "r");
+    if (!f)
+        return -errno;
+
+    char buf[64];
+    if (!fgets(buf, sizeof(buf), f)) {
+        fclose(f);
+        return -EIO;
+    }
+    fclose(f);
+
+    char *eq = strchr(buf, '=');
+    const char *start = eq ? eq + 1 : buf;
+    *config = strtoull(start, NULL, 0);
+    return 0;
+}
+
+static int resolve_event_path(const char *event, char *path, size_t len)
+{
+    int written = snprintf(path, len,
+                           "/sys/bus/event_source/devices/power/events/%s",
+                           event);
+    if (written < 0 || (size_t)written >= len)
+        return -ENAMETOOLONG;
+    return 0;
+}
+
+static int get_event_config(const char *const *candidates, unsigned long long *config)
+{
+    char path[PATH_MAX];
+    for (const char *const *ev = candidates; *ev; ev++) {
+        if (resolve_event_path(*ev, path, sizeof(path)) != 0)
+            continue;
+        if (read_event_config(path, config) == 0)
+            return 0;
+    }
+    return -ENOENT;
+}
+
+static int open_rapl_event(int pmu_type, __u64 config)
+{
+    struct perf_event_attr attr = {
+        .type = pmu_type,
+        .size = sizeof(struct perf_event_attr),
+        .config = config,
+        .disabled = 0,
+        .exclude_kernel = 0,
+        .exclude_hv = 0,
+        .read_format = PERF_FORMAT_TOTAL_TIME_ENABLED |
+                        PERF_FORMAT_TOTAL_TIME_RUNNING,
+    };
+
+    int fd = perf_event_open(&attr, -1, 0, -1, 0);
+    if (fd < 0)
+        fprintf(stderr, "ERROR: perf_event_open for config %#llx failed: %s\n",
+                (unsigned long long)config, strerror(errno));
+    return fd;
+}
+
+static void close_perf_events(int *fds, size_t count)
+{
+    for (size_t i = 0; i < count; i++) {
+        if (fds[i] >= 0)
+            close(fds[i]);
+    }
+}
+
+int main(int argc, char **argv)
+{
     struct bpf_object *obj;
     struct bpf_program *prog;
-    struct bpf_link *link = NULL;
     int stats_map_fd;
     int temps_map_fd;
     int config_map_fd;
-    int timer_prog_fd;
+    int perf_map_fd;
     int err;
-    
+    int perf_fds[PERF_EVENT_COUNT] = { [0 ... (PERF_EVENT_COUNT - 1)] = -1 };
+
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
-    
+
+    if (setrlimit(RLIMIT_MEMLOCK,
+                  &(struct rlimit){ .rlim_cur = RLIM_INFINITY, .rlim_max = RLIM_INFINITY })) {
+        perror("setrlimit(RLIMIT_MEMLOCK)");
+        return 1;
+    }
+
     const char *bpf_obj_path = argc > 1 ? argv[1] : "repl_stats_interval.bpf.o";
-    
+
     obj = bpf_object__open_file(bpf_obj_path, NULL);
     if (libbpf_get_error(obj)) {
         fprintf(stderr, "ERROR: failed to open BPF object at %s\n", bpf_obj_path);
         fprintf(stderr, "Make sure you run this from the build/ directory or provide the path\n");
         return 1;
     }
-    
+
     err = bpf_object__load(obj);
     if (err) {
         fprintf(stderr, "ERROR: failed to load BPF object: %d\n", err);
         goto cleanup;
     }
 
+    long cpu_cnt = sysconf(_SC_NPROCESSORS_ONLN);
+    if (cpu_cnt < 1)
+        cpu_cnt = 1;
+    if (cpu_cnt > MAX_CORE_TEMPS)
+        cpu_cnt = MAX_CORE_TEMPS;
+
     config_map_fd = bpf_object__find_map_fd_by_name(obj, "rapl_config_map");
     if (config_map_fd >= 0) {
         __u32 cfg_key = 0;
-        struct rapl_config cfg = {};
-        long cpu_cnt = sysconf(_SC_NPROCESSORS_ONLN);
-        if (cpu_cnt < 1)
-            cpu_cnt = 1;
-        if (cpu_cnt > MAX_CORE_TEMPS)
-            cpu_cnt = MAX_CORE_TEMPS;
-        cfg.core_count = cpu_cnt;
+        struct rapl_config cfg = {
+            .core_count = cpu_cnt,
+        };
         if (bpf_map_update_elem(config_map_fd, &cfg_key, &cfg, BPF_ANY))
             fprintf(stderr, "WARNING: failed to set core_count config: %s\n",
                     strerror(errno));
     } else {
         fprintf(stderr, "WARNING: rapl_config_map not found, using defaults\n");
     }
-    
+
     stats_map_fd = bpf_object__find_map_fd_by_name(obj, "rapl_stats_map");
     if (stats_map_fd < 0) {
         fprintf(stderr, "ERROR: failed to find rapl_stats_map\n");
@@ -72,59 +180,96 @@ int main(int argc, char **argv) {
         fprintf(stderr, "ERROR: failed to find core_temp_map\n");
         goto cleanup;
     }
-    
+
+    perf_map_fd = bpf_object__find_map_fd_by_name(obj, "perf_event_map");
+    if (perf_map_fd < 0) {
+        fprintf(stderr, "ERROR: failed to find perf_event_map\n");
+        goto cleanup;
+    }
+
     const char *stats_pin_path = "/sys/fs/bpf/rapl_stats";
     const char *temps_pin_path = "/sys/fs/bpf/rapl_temps";
     err = bpf_obj_pin(stats_map_fd, stats_pin_path);
-    if (err && errno != EEXIST) {
-        fprintf(stderr, "WARNING: failed to pin map to %s: %s\n", stats_pin_path, strerror(errno));
-    } else {
+    if (err && errno != EEXIST)
+        fprintf(stderr, "WARNING: failed to pin map to %s: %s\n", stats_pin_path,
+                strerror(errno));
+    else
         printf("BPF map pinned to: %s\n", stats_pin_path);
-    }
 
     err = bpf_obj_pin(temps_map_fd, temps_pin_path);
-    if (err && errno != EEXIST) {
-        fprintf(stderr, "WARNING: failed to pin map to %s: %s\n", temps_pin_path, strerror(errno));
-    } else {
+    if (err && errno != EEXIST)
+        fprintf(stderr, "WARNING: failed to pin map to %s: %s\n", temps_pin_path,
+                strerror(errno));
+    else
         printf("BPF map pinned to: %s\n", temps_pin_path);
+
+    const char *const pkg_candidates[] = { "energy-pkg", "energy-package", NULL };
+    const char *const core_candidates[] = { "energy-cores", "energy-core", "energy-pp0", NULL };
+    unsigned long long pmu_type = 0;
+    unsigned long long pkg_config = 0;
+    unsigned long long core_config = 0;
+
+    if (read_file_u64("/sys/bus/event_source/devices/power/type", &pmu_type) != 0) {
+        fprintf(stderr, "WARNING: power PMU not available (is Intel RAPL supported?)\n");
+        pmu_type = 0;
     }
-    
+
+    if (pmu_type) {
+        if (get_event_config(pkg_candidates, &pkg_config) == 0)
+            perf_fds[PERF_IDX_PACKAGE] = open_rapl_event(pmu_type, pkg_config);
+        else
+            fprintf(stderr, "WARNING: unable to resolve package energy event\n");
+
+        if (get_event_config(core_candidates, &core_config) == 0)
+            perf_fds[PERF_IDX_CORE] = open_rapl_event(pmu_type, core_config);
+        else
+            fprintf(stderr, "WARNING: unable to resolve core energy event\n");
+    }
+
+    for (__u32 i = 0; i < PERF_EVENT_COUNT; i++) {
+        if (perf_fds[i] < 0)
+            continue;
+        if (bpf_map_update_elem(perf_map_fd, &i, &perf_fds[i], BPF_ANY))
+            fprintf(stderr, "WARNING: failed to set perf event %u: %s\n", i,
+                    strerror(errno));
+    }
+
     prog = bpf_object__find_program_by_name(obj, "start_timer");
     if (!prog) {
         fprintf(stderr, "ERROR: failed to find start_timer program\n");
         goto cleanup;
     }
-    
-    timer_prog_fd = bpf_program__fd(prog);
+
+    int timer_prog_fd = bpf_program__fd(prog);
     if (timer_prog_fd < 0) {
         fprintf(stderr, "ERROR: failed to get program fd\n");
         goto cleanup;
     }
-    
+
     LIBBPF_OPTS(bpf_test_run_opts, opts);
     err = bpf_prog_test_run_opts(timer_prog_fd, &opts);
     if (err || opts.retval) {
-        fprintf(stderr, "ERROR: failed to start BPF timer: %d (retval: %d)\n", err, opts.retval);
+        fprintf(stderr, "ERROR: failed to start BPF timer: %d (retval: %d)\n",
+                err, opts.retval);
         goto cleanup;
     }
-    
-    printf("RAPL Stats Updater started with BPF timer (100ms interval)\n");
+
+    printf("RAPL Stats Updater streaming Intel RAPL data every 100ms (BPF timer)\n");
     printf("SCX can read stats from BPF map at: %s\n", stats_pin_path);
-    printf("Per-core temps available at: %s\n", temps_pin_path);
+    printf("Per-core temps available at: %s (still randomized)\n", temps_pin_path);
     printf("Press Ctrl+C to stop.\n\n");
-    
-    while (keep_running) {
-        pause(); 
-    }
-    
-    printf("\n\nStopping...\n");
-    
-    unlink("/sys/fs/bpf/rapl_stats");
-    unlink("/sys/fs/bpf/rapl_temps");
-    
+
+    while (keep_running)
+        pause();
+
+    printf("\nStopping...\n");
+
+    unlink(stats_pin_path);
+    unlink(temps_pin_path);
+
 cleanup:
-    if (link)
-        bpf_link__destroy(link);
-    bpf_object__close(obj);
+    close_perf_events(perf_fds, PERF_EVENT_COUNT);
+    if (obj)
+        bpf_object__close(obj);
     return err != 0;
 }
--- src/bpf-stats-updater/repl_stats_interval.bpf.c
+++ src/bpf-stats-updater/repl_stats_interval.bpf.c
@@ -1,8 +1,15 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 #include "../include/rapl_stats.h"
 
 char LICENSE[] SEC("license") = "GPL";
+
+#define PERF_IDX_PACKAGE 0
+#define PERF_IDX_CORE    1
+#define PERF_EVENT_COUNT 2
+#define SAMPLE_INTERVAL_NS 100000000ULL
+#define MICROJOULES_PER_JOULE 1000000ULL
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -25,6 +32,13 @@ struct {
     __type(value, __u32);
 } core_temp_map SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, PERF_EVENT_COUNT);
+    __type(key, __u32);
+    __type(value, __u32);
+} perf_event_map SEC(".maps");
+
 struct timer_wrapper {
     struct bpf_timer timer;
 };
@@ -37,14 +51,60 @@ struct {
 } timer_map SEC(".maps");
 
 static __u64 seed = 12345;
+static __u64 last_pkg_energy;
+static __u64 last_core_energy;
+static __u64 last_timestamp;
 
-static __u64 rand_u64(void) {
+static __u64 rand_u64(void)
+{
     seed = seed * 1103515245 + 12345;
     return seed;
 }
 
-static __u32 rand_range(__u32 min, __u32 max) {
+static __u32 rand_range(__u32 min, __u32 max)
+{
     return min + (rand_u64() % (max - min + 1));
+}
+
+static __u64 energy_delta(__u64 curr, __u64 prev)
+{
+    if (!prev)
+        return 0;
+    if (curr >= prev)
+        return curr - prev;
+    return (1ULL << 32) - prev + curr;
+}
+
+static int read_energy(__u32 idx, __u64 *value)
+{
+    struct bpf_perf_event_value data = {};
+    int ret;
+
+    ret = bpf_perf_event_read_value(&perf_event_map, idx, &data,
+                                    sizeof(data));
+    if (ret)
+        return ret;
+    *value = data.counter;
+    return 0;
+}
+
+static __u64 compute_power(__u64 delta_energy_uj, __u64 delta_ns)
+{
+    if (!delta_ns || !delta_energy_uj)
+        return 0;
+    return (delta_energy_uj * 1000ULL) / delta_ns;
+}
+
+static void update_core_temps(__u32 count)
+{
+#pragma clang loop unroll(disable)
+    for (int i = 0; i < MAX_CORE_TEMPS; i++) {
+        if (i >= count)
+            break;
+        __u32 idx = i;
+        __u32 temp = rand_range(38, 82);
+        bpf_map_update_elem(&core_temp_map, &idx, &temp, BPF_ANY);
+    }
 }
 
 static int timer_callback(void *map, int *key, struct bpf_timer *timer)
@@ -54,39 +114,44 @@ static int timer_callback(void *map, int *key, struct bpf_timer *timer)
     struct rapl_stats *stats;
     struct rapl_config *cfg;
     __u32 core_count = MAX_CORE_TEMPS;
+    __u64 now = bpf_ktime_get_ns();
+    __u64 delta_ns = last_timestamp ? (now - last_timestamp) : SAMPLE_INTERVAL_NS;
 
     stats = bpf_map_lookup_elem(&rapl_stats_map, &stats_key);
     if (!stats)
-        return 0;
+        goto out;
 
     cfg = bpf_map_lookup_elem(&rapl_config_map, &config_key);
     if (cfg && cfg->core_count > 0 && cfg->core_count <= MAX_CORE_TEMPS)
         core_count = cfg->core_count;
-    
-    stats->timestamp = bpf_ktime_get_ns();
-    stats->delta_time = 100000000;
-    
-    stats->package_power = rand_range(15, 95);
-    stats->core_power = rand_range(10, 65);
-    
-    stats->package_energy = (stats->package_power * 100) / 1000;
-    stats->core_energy = (stats->core_power * 100) / 1000;
-    
-    stats->package_temp = rand_range(40, 85);
+
+    stats->timestamp = now;
+    stats->delta_time = delta_ns;
     stats->core_count = core_count;
-#pragma clang loop unroll(disable)
-    for (int i = 0; i < MAX_CORE_TEMPS; i++) {
-        if (i >= core_count)
-            break;
-        __u32 idx = i;
-        __u32 temp = rand_range(38, 82);
-        bpf_map_update_elem(&core_temp_map, &idx, &temp, BPF_ANY);
+
+    __u64 pkg_energy_raw = 0;
+    if (!read_energy(PERF_IDX_PACKAGE, &pkg_energy_raw)) {
+        __u64 delta_energy = energy_delta(pkg_energy_raw, last_pkg_energy);
+        stats->package_power = compute_power(delta_energy, delta_ns);
+        stats->package_energy = pkg_energy_raw / MICROJOULES_PER_JOULE;
+        last_pkg_energy = pkg_energy_raw;
     }
-    
-    stats->tdp = rand_range(35, 125);
-    
-    bpf_timer_start(timer, 100000000, 0);
-    
+
+    __u64 core_energy_raw = 0;
+    if (!read_energy(PERF_IDX_CORE, &core_energy_raw)) {
+        __u64 delta_energy = energy_delta(core_energy_raw, last_core_energy);
+        stats->core_power = compute_power(delta_energy, delta_ns);
+        stats->core_energy = core_energy_raw / MICROJOULES_PER_JOULE;
+        last_core_energy = core_energy_raw;
+    }
+
+    stats->package_temp = rand_range(40, 85);
+    update_core_temps(core_count);
+    stats->tdp = stats->package_power;
+
+out:
+    last_timestamp = now;
+    bpf_timer_start(timer, SAMPLE_INTERVAL_NS, 0);
     return 0;
 }
 
@@ -96,20 +161,19 @@ int start_timer(void *ctx)
     __u32 key = 0;
     struct timer_wrapper *tw;
     int ret;
-    
+
     tw = bpf_map_lookup_elem(&timer_map, &key);
     if (!tw)
         return -1;
-    
-    ret = bpf_timer_init(&tw->timer, &timer_map, 0); 
+
+    ret = bpf_timer_init(&tw->timer, &timer_map, 0);
     if (ret)
         return ret;
-    
+
     ret = bpf_timer_set_callback(&tw->timer, timer_callback);
     if (ret)
         return ret;
-    
-    ret = bpf_timer_start(&tw->timer, 100000000, 0);
-    
+
+    ret = bpf_timer_start(&tw->timer, SAMPLE_INTERVAL_NS, 0);
     return ret;
 }
