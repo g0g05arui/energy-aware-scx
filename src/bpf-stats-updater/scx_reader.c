c00962a83488b805bd12b4cfbe11956744aa01f8
Mihai Indreias <mihai.indreias@epfl.ch>
2025-12-11 16:15:58 +0000

    add threshold generation and core_state map

--- src/bpf-stats-updater/scx_reader.c
+++ src/bpf-stats-updater/scx_reader.c
@@ -4,16 +4,34 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 #include <bpf/bpf.h>
 #include "../include/rapl_stats.h"
+#include "../include/core_state.h"
+
+static const char *state_to_str(enum core_status state)
+{
+    switch (state) {
+    case CORE_COLD:
+        return "cold";
+    case CORE_WARM:
+        return "warm";
+    case CORE_HOT:
+        return "hot";
+    default:
+        return "?";
+    }
+}
 
 int main(int argc, char **argv) {
     const char *pin_path = "/sys/fs/bpf/rapl_stats";
     const char *temps_pin_path = "/sys/fs/bpf/rapl_temps";
     const char *temp_count_pin_path = "/sys/fs/bpf/rapl_temp_count";
+    const char *state_pin_path = "/sys/fs/bpf/rapl_core_states";
     int map_fd;
     int temps_fd;
     int temp_count_fd;
+    int state_fd;
     struct rapl_stats stats;
     __u32 key = 0;
     int err;
@@ -24,6 +42,12 @@ int main(int argc, char **argv) {
                 pin_path, strerror(errno));
         fprintf(stderr, "Make sure rapl_stats_updater is running!\n");
         return 1;
+    }
+
+    state_fd = bpf_obj_get(state_pin_path);
+    if (state_fd < 0) {
+        fprintf(stderr, "WARNING: failed to open core state map at %s: %s\n",
+                state_pin_path, strerror(errno));
     }
     
     temps_fd = bpf_obj_get(temps_pin_path);
@@ -71,10 +95,19 @@ int main(int argc, char **argv) {
     for (unsigned int i = 0; i < temp_count; i++) {
         __u32 idx = i;
         __u32 temp = 0;
-        if (bpf_map_lookup_elem(temps_fd, &idx, &temp) == 0)
-            printf("%u", temp);
-        else
-            printf("?");
+        enum core_status state = CORE_COLD;
+        const char *state_str = "?";
+        bool has_temp = bpf_map_lookup_elem(temps_fd, &idx, &temp) == 0;
+        if (has_temp) {
+            if (state_fd >= 0 &&
+                bpf_map_lookup_elem(state_fd, &idx, &state) == 0)
+                state_str = state_to_str(state);
+            else
+                state_str = "n/a";
+            printf("%u(%s)", temp, state_str);
+        } else {
+            printf("?(n/a)");
+        }
         if (i + 1 != temp_count)
             printf(", ");
     }
@@ -86,5 +119,7 @@ int main(int argc, char **argv) {
     close(temps_fd);
     if (temp_count_fd >= 0)
         close(temp_count_fd);
+    if (state_fd >= 0)
+        close(state_fd);
     return 0;
 }
